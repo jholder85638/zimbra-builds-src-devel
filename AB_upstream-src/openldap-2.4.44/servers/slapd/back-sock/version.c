84993f6905399285cb8f9c3e20bb16d53de29ecb
root <root@zcs86.ubuntu14.johholder.net>
2017-12-11 23:56:02 -0700

    resync

---
+++ AB_upstream-src/openldap-2.4.44/servers/slapd/back-sock/version.c
@@ -0,0 +1,22 @@
+/* This work is part of OpenLDAP Software <http://www.openldap.org/>.
+ *
+ * Copyright 1998-2016 The OpenLDAP Foundation.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only as authorized by the OpenLDAP
+ * Public License.
+ *
+ * A copy of this license is available in the file LICENSE in the
+ * top-level directory of the distribution or, alternatively, at
+ * <http://www.OpenLDAP.org/license.html>.
+ */
+
+static const char copyright[] =
+"Copyright 1998-2016 The OpenLDAP Foundation.  All rights reserved.\n"
+"COPYING RESTRICTIONS APPLY\n";
+
+static const char __Version[] =
+"@(#) $OpenLDAP: back_sock 2.4.44 (" __DATE__ " " __TIME__ ") $\n"
+"\troot@irina.telenet-ops.be:/opt/zimbra-builds-src-devel/AB_upstream-src/openldap-2.4.44/servers/slapd/back-sock\n";
+
